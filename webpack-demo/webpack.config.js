3f00e970be47f2f43a34c574516d89d5dcb07380
Cisco <ciscophrenzy@gmail.com>
2023-03-28 16:24:38 -0600

    Install webpack

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,23 @@
+const path =require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+          template: './src/index.html'
+        }),
+      ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+};
